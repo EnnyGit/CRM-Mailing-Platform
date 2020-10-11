a9f39c5b283c2985b9e00e7712e6dabc53b8e511
Enny <46939885+EnnyGit@users.noreply.github.com>
2020-10-12 00:32:43 +0200

    Fixed Project structure

--- CRM Project/Program.cs
+++ CRM Project/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace CRM_Mailing_Platform
+namespace CRM_Project
 {
     public class Program
     {

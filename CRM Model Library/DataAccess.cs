f86e7b8ceb00e6b121d53c899bd1355812093c8f
Enny <46939885+EnnyGit@users.noreply.github.com>
2020-11-13 12:13:49 +0100

    Added DataAccess class
    - Moved models into their own folder
    - Added comments for DataAccess class

---
+++ CRM Model Library/DataAccess.cs
@@ -0,0 +1,51 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace CRM_Model_Library
+{
+    public class DataAccess
+    {
+        /// <summary>
+        /// This Method goes to a table and get information out of it.
+        /// </summary>
+        /// <typeparam name="T">
+        /// A generic Type which should be a model object.
+        /// </typeparam>
+        /// <typeparam name="U">
+        /// A generic Type for the parameters input.
+        /// </typeparam>
+        /// <param name="sql">
+        /// This is the statement that says "SELECT * FROM 'Table'"
+        /// </param>
+        /// <param name="parameters">
+        /// A parameter of any type.
+        /// </param>
+        /// <param name="connectionString">
+        /// How we connect to our Database.
+        /// </param>
+        /// <returns>
+        /// Returns a List of Type T models.
+        /// </returns>
+
+        public static List<T> LoadData<T, U>(string sql, U parameters, string connectionString)
+        {
+            // Opens a connection to our Database, it does it in a Using statement so that when
+            // we're done it will close it properly.
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                // Query of Type <T> will map the data comming out into a model of type <T>. It will be 
+                // a set of those, one per row.
+                List<T> rows = connection.Query<T>(sql, parameters).ToList();
+
+                // Return is seperated from the query, because it will be easier to debug.
+                return rows;
+            }
+
+        }
+    }
+}

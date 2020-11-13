610fc7eabdb6474642645b8274846bd9ae3f94c5
Enny <46939885+EnnyGit@users.noreply.github.com>
2020-11-13 11:04:09 +0100

    Added data_access code
    Not done yet

---
+++ CRM Model Library/ClientData.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_Model_Library
+{
+    class ClientData : IClientData
+    {
+        private readonly ISqlDataAccess _db;
+
+        // Now this class has access to IsqlDataAccess.
+        public ClientData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task<List<ClientModel>> GetClients()
+        {
+            string sql = "select * from dbo.Client";
+
+            return _db.LoadData<ClientModel, dynamic>(sql, new { });
+        }
+
+        public Task InsertClient(ClientModel person)
+        {
+            string sql = @"insert into dbo.People (FirstName, Lastname, EmailAddress)
+                            values (@FirstName, @LastName, @EmailAddress);";
+
+            return _db.SaveData(sql, person);
+        }
+    }
+}

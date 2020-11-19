1eaad3cd4b0537ddd5e23b345532326f13448540
Enny <46939885+EnnyGit@users.noreply.github.com>
2020-11-19 14:46:30 +0100

    Fixed Merge errors

--- CRM_Blazor_Server/ViewModels/DisplayClientModel.cs
+++ CRM_Blazor_Server/ViewModels/DisplayClientModel.cs
@@ -11,15 +11,6 @@ namespace CRM_Blazor_Server.ViewModels
         [Required]
         [StringLength(15, ErrorMessage = "First Name is too long.")]
         [MinLength(5, ErrorMessage = "First Name is too short.")]
-        public string FirstName { get; set; }
-
-        [Required]
-        [StringLength(15, ErrorMessage = "First Name is too long.")]
-        [MinLength(5, ErrorMessage = "First Name is too short.")]
-        public string LastName { get; set; }
-
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; }
+        public string Name { get; set; }
     }
 }

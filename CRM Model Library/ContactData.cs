e0627b6530078792553617f2c66837a9a2331503
jonas de boer <jonasdeboer02@gmail.com>
2020-12-01 19:52:04 +0100

    Can add contacts (sort of)

--- CRM Model Library/ContactData.cs
+++ CRM Model Library/ContactData.cs
@@ -23,8 +23,8 @@ namespace CRM_Model_Library
 
         public Task InsertContact(ContactModel contact)
         {
-            string sql = @"INSERT INTO dbo.client(FirstName, LastName, Email)
-                           values (@FirstName, @LastName, @Email);";
+            string sql = @"INSERT INTO dbo.Contact(FirstName, LastName, EmailAddress)
+                           values (@FirstName, @LastName, @EmailAddress);";
 
             return _db.SaveData(sql, contact);
         }
@@ -34,6 +34,11 @@ namespace CRM_Model_Library
                             WHERE cc.ContactId = @contact.ContactId";
             return _db.LoadData<ClientModel, dynamic>(sql, new { });
 
+        }
+        public Task<List<ContactModel>> GetSelectContacts(int start, int rows)
+        {
+            string sql = "SELECT * FROM dbo.Contact ORDER BY ContactId OFFSET @start ROWS FETCH NEXT @rows ROWS ONLY";
+            return _db.LoadData<ContactModel, dynamic>(sql, new { });
         }
     }
 }

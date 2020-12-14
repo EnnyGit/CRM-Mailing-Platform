51f34d697ddd091c50f5c63865a704c414bd201b
jonas de boer <jonasdeboer02@gmail.com>
2020-12-14 11:41:01 +0100

    changed equals

--- CRM Model Library/Models/ClientModel.cs
+++ CRM Model Library/Models/ClientModel.cs
@@ -5,5 +5,18 @@ public class ClientModel
     public int ClientId { get; set; }
 
     public string Name { get; set; }
+
+    public override bool Equals(object obj)
+    {
+        if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+        {
+            return false;
+        }
+        else
+        {
+            ClientModel c = (ClientModel)obj;
+            return this.ClientId == c.ClientId;
+        }
+    }
 }
 

cb820586abe774dadb63313eef6f853a3aa42ac0
Enny <46939885+EnnyGit@users.noreply.github.com>
2020-12-17 12:36:18 +0100

    Fixed error

--- CRM Model Library/MailChimp/MailChimpController.cs
+++ CRM Model Library/MailChimp/MailChimpController.cs
@@ -35,20 +35,20 @@ namespace CRM_Model_Library
             catch (Exception ex)
             {
                 //TODO: Error message "httpStatusCodeResult(HttpStatusCode.ServiceUnavailable, ex.Message)"
-                var model = await Manager.Campaigns.GetAllAsync(options);
+                var model = await ApiKeyMailChimp.Manager.Campaigns.GetAllAsync(options);
                 return model;
             }
         }
 
         public async Task<IEnumerable<MailChimp.Net.Models.List>> GetLists()
         {
-            var model = await Manager.Lists.GetAllAsync();
+            var model = await ApiKeyMailChimp.Manager.Lists.GetAllAsync();
             return model;
         }
 
         public async Task<IEnumerable<MailChimp.Net.Models.Member>> GetMembers()
         {
-            var model = await Manager.Members.GetAllAsync("0756e04de0");
+            var model = await ApiKeyMailChimp.Manager.Members.GetAllAsync("0756e04de0");
             return model;
         }
     }

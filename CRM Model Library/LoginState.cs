994006c531237e480c90a612245da890d995c4d9
jonas de boer <jonasdeboer02@gmail.com>
2021-01-08 08:05:21 +0100

    start login popup

---
+++ CRM Model Library/LoginState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRM_Model_Library
+{
+    public class LoginState
+    {
+        public bool IsLoggedIn { get; set; }
+        public string username { get; set; }
+        public event Action OnChange;
+
+        public void SetLogin(bool login, string user)
+        {
+            IsLoggedIn = login;
+            username = user;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged()
+        {
+            OnChange?.Invoke();
+        }
+    }
+}

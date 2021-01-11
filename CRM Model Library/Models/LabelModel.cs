90574caae6e8ab7bdbd3d26d4fd495217c13d41d
jonas de boer <jonasdeboer02@gmail.com>
2021-01-11 13:27:13 +0100

    updated routes and can now add labels to contact

--- CRM Model Library/Models/LabelModel.cs
+++ CRM Model Library/Models/LabelModel.cs
@@ -3,6 +3,18 @@ public class LabelModel
 	public int LabelId { get; set; }
 
 	public string LabelName { get; set; }
+    public override bool Equals(object obj)
+    {
+        if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+        {
+            return false;
+        }
+        else
+        {
+            LabelModel c = (LabelModel)obj;
+            return this.LabelId == c.LabelId;
+        }
+    }
 }
 
 
